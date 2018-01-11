acf5798304272569f214d130c43768c3a4e9af25
RyanJaime <jaimeryan139@yahoo.com>
2018-01-10 16:30:56 -0800

    tiles grow to indicate which notes are coming

---
+++ Assets/Scripts/indicatorGrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indicatorGrow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 20))
+        {
+            print("Found an object - distance: " + hit.distance);
+            transform.localScale += new Vector3(0.0028f, 0, 0.0028f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0, 1, 0);
+        }
+    }
+}
