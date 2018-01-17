705315a893719d747d959705417c1b7101e87c68
Ryan Jackson Jaime <rjaime@crm.ucsc.edu>
2018-01-16 22:55:49 -0800

    xbox controls

---
+++ Assets/Scripts/Player/Rotate.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float speed = 15;
+    private int timesHitHorizontal = 0;
+    private int timesHitVertical = 0;
+    private int timesHitRolling = 0;
+
+    //0 = front
+    //1 = right
+    //2 = back
+    //3 = left
+    private int frontFace = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            timesHitHorizontal--;
+            changeFace(true);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            timesHitHorizontal++;
+            changeFace(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            timesHitVertical++;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            timesHitVertical--;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            timesHitRolling++;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            timesHitRolling--;
+        }
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(timesHitVertical * 90, timesHitHorizontal * 90, timesHitRolling * 90), Time.deltaTime * speed);
+
+    }
+
+    void changeFace(bool increase)
+    {
+        if (increase) { frontFace++; }
+        else { frontFace--; }
+
+        if (frontFace > 3) { frontFace -= 4; }
+        if (frontFace < 0) { frontFace += 4; }
+        print(frontFace);
+    }
+}
--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -6,82 +6,127 @@ public class movement : MonoBehaviour {
 
 	private float mx;
     Vector3 neutralPosition;
-    // Use this for initialization
 
-    //RaycastHit hit;
-    //public float heightAbove;
-    //float heightAboveRoad = 0.0f;
-    //float diff = 0.0f;
+    private bool inNeutral;
+
+    // Rotate 
+    public float speed = 15;
+    private int timesHitHorizontal = 0;
+    private int timesHitVertical = 0;
+    private int timesHitRolling = 0;
+    private int frontFace = 0;
+
 
     void Start () {
 		//hit = new RaycastHit ();
 		mx = 1;
+        inNeutral = true;
         Vector3 neutralPosition = transform.position; // new Vector3(0, 0.5f, 0);
         print(transform.position + " " + neutralPosition);
 	}
-
 
     // Update is called once per frame
     void Update () {
         float moveX = Input.GetAxis("MovementX");
         float moveY = Input.GetAxis("MovementY");
+
         //float rotateX = Input.GetAxis("RotateX");
         //float rotateY = Input.GetAxis("RotateY");
         //Rotate(rotateX, rotateY);
         //print("JP");
-        if (transform.position.x == neutralPosition.x && transform.position.z == neutralPosition.z) //Equals(object other)
-        {
-            //print("yep");
+        /*
+        //if (transform.position.x == neutralPosition.x && transform.position.z == neutralPosition.z) //Equals(object other)
+        //{
             if (Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("MovementX") > 0)
             {
-
-                //if (transform.position == neutralPosition)
-                //{
-                    transform.position = new Vector3(transform.position.x + mx, transform.position.y, transform.position.z);
-               // }
+                changeFace(true);
+                transform.position = new Vector3(transform.position.x + mx, transform.position.y, transform.position.z);
             }
             else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("MovementX") < 0)
-            { // (Mathf.Abs(Input.GetAxis("MovementX")) > Mathf.Abs(Input.GetAxis("MovementY")) && Input.GetAxis("MovementX") < 0)
-   
-                //if (transform.position == neutralPosition)
-               // {
-                    transform.position = new Vector3(transform.position.x - mx, transform.position.y, transform.position.z);
-              //  }
+            { 
+                changeFace(false);
+                transform.position = new Vector3(transform.position.x - mx, transform.position.y, transform.position.z);
             }
             else if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("MovementY") < 0)
             {
-            
-               // if (transform.position == neutralPosition)
-                //{
-                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + mx);
-                //}
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + mx);
             }
             else if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("MovementY") > 0)
             {
-             
-               // if (transform.position == neutralPosition)
-                //{
-
-                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - mx);
-                //}
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - mx);
             }
             else
             {
                 transform.position = neutralPosition;
             }
 
-        }
-        
+        // }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            transform.position = neutralPosition;
-        }
-        else
         {
             //transform.position = neutralPosition;
         }
+        else
+        {
+            transform.position = neutralPosition;
+        }
+        */
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || (Input.GetAxis("MovementX") == 0 && Input.GetAxis("MovementY") == 0))
+        {
+
+            inNeutral = true;
+            timesHitRolling = 0;
+            timesHitVertical = 0;
+            transform.position = neutralPosition;
+            //this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        //else if (inNeutral)
+        //{
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("MovementX") > 0)
+            {
+                inNeutral = false;
+                timesHitRolling = -1;
+                timesHitVertical = 0;
+                changeFace(true);
+                transform.position = new Vector3(mx, transform.position.y, 0);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("MovementX") < 0)
+            {
+                inNeutral = false;
+                timesHitRolling = 1;
+                timesHitVertical = 0;
+                changeFace(false);
+                transform.position = new Vector3(-mx, transform.position.y, 0);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("MovementY") < 0)
+            {
+                inNeutral = false;
+                timesHitRolling = 0;
+                timesHitVertical = 1;
+                transform.position = new Vector3(0, transform.position.y, mx);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("MovementY") > 0)
+            {
+                inNeutral = false;
+                timesHitRolling = 0;
+                timesHitVertical = -1;
+                transform.position = new Vector3(0, transform.position.y, -mx);
+            }
+        //}
         
-	}
+
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(timesHitVertical * 90, timesHitHorizontal * 90, timesHitRolling * 90), Time.deltaTime * speed);
+        
+    }
+
+    void changeFace(bool increase)
+    {
+        if (increase) { frontFace++; }
+        else { frontFace--; }
+
+        if (frontFace > 3) { frontFace -= 4; }
+        if (frontFace < 0) { frontFace += 4; }
+        //print(frontFace);
+    }
 
     public void Rotate(float rotateX, float rotateY)
     {
